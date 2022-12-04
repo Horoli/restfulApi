d86f13b838e894afa894b23a4d836919b141c2a5
Horoli <sunha321@gmail.com>
2022-12-04 20:08:02 +0900

    modified : server port

--- server.cjs
+++ server.cjs
@@ -101,4 +101,4 @@ server.all('/allRoute', async (req, res) => {
 // server.register(user, { prefix: '/user' });
 
 // server.listen(3000);
-server.listen(3000, "1.1.1.1");
+server.listen(3000, "0.0.0.0");
