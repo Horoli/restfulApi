48aedfa639d7c3038eea70cc75fa0a471b621624
Horoli <sunha321@gmail.com>
2023-01-12 09:28:18 +0900

    add

---
+++ src/routes/get.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+    
+    'POST /get': {
+        async handler(req, rep) {
+            return {
+                "data": "ok"
+            }
+        }
+    },
+    'GET /get': {
+        async handler(req, rep) {
+            return json
+        }
+    }
+}
--- src/routes/hello.js
+++ src/routes/hello.js
@@ -1,4 +1,3 @@
-
 module.exports = {
     'POST /hello': {
         async handler(req, rep) {
@@ -9,9 +8,34 @@ module.exports = {
     },
     'GET /hello': {
         async handler(req, rep) {
-            return {
-                "data": "ok"
-            }
+            return json
         }
     }
 }
+
+
+
+    // "quest": [
+    //     {
+    //         "kind": "",
+    //         "description": "1 + 1 = ?",
+    //         "choise": [
+    //             "1",
+    //             "2",
+    //             "3",
+    //             "4"
+    //         ],
+    //         "correct": "2"
+    //     },
+    //     {
+    //         "kind": "",
+    //         "description": "1 + 2 = ?",
+    //         "choise": [
+    //             "1",
+    //             "2",
+    //             "3",
+    //             "4"
+    //         ],
+    //         "correct": "3"
+    //     }
+    // ]
