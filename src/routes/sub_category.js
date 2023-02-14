c9e9e0b3dc78c5a3d00615adf9aa1cf35a4bbdaa
Horoli <sunha321@gmail.com>
2023-02-15 00:43:29 +0900

    update : type delete

--- src/routes/sub_category.js
+++ src/routes/sub_category.js
@@ -1,64 +1,64 @@
 const Database = require("../datebase");
 
 module.exports = {
-    "POST /subcategory": {
-        middlewares: ["auth"],
-        async handler(req, rep) {
-            const id = "id";
-            const maincategory = "maincategory";
-            const subcategory = "subcategory";
+  "POST /subcategory": {
+    middlewares: ["auth"],
+    async handler(req, rep) {
+      const id = "id";
+      const maincategory = "maincategory";
+      const subcategory = "subcategory";
 
-            const titleData = req.body[subcategory];
-            const idData = req.body[id].replace(/-/g, "");
-            const maincategoryData = req.body[maincategory];
-            duplicateCheck = false;
+      const titleData = req.body[subcategory];
+      const idData = req.body[id].replace(/-/g, "");
+      const maincategoryData = req.body[maincategory];
+      duplicateCheck = false;
 
-            const subCategoryCol =
-                Database.sharedInstance().getCollection(subcategory);
+      const subCategoryCol =
+        Database.sharedInstance().getCollection(subcategory);
 
-            const objValues = Object.values(subCategoryCol["$dataset"]);
-            console.log("objValues", objValues);
+      const objValues = Object.values(subCategoryCol["$dataset"]);
+      console.log("objValues", objValues);
 
-            // TODO : keys를 돌면서 type이 중복되는지 체크
-            objValues.forEach(function (value) {
-                // TODO : 중복된 데이터가 있으면 duplicateCheck를 true로
-                if (value[subcategory] === titleData) {
-                    duplicateCheck = true;
-                }
-            });
+      // TODO : keys를 돌면서 type이 중복되는지 체크
+      objValues.forEach(function (value) {
+        // TODO : 중복된 데이터가 있으면 duplicateCheck를 true로
+        if (value[subcategory] === titleData) {
+          duplicateCheck = true;
+        }
+      });
 
-            // TODO : 중복된 데이터가 있으면 error return
-            if (duplicateCheck === true) {
-                const error = new Error("this subCategory already has data");
-                error.status = 403;
-                return error;
-            }
+      // TODO : 중복된 데이터가 있으면 error return
+      if (duplicateCheck === true) {
+        const error = new Error("this subCategory already has data");
+        error.status = 403;
+        return error;
+      }
 
-            if (duplicateCheck !== true) {
-                subCategoryCol.set(idData, {
-                    id: idData,
-                    maincategory: maincategoryData,
-                    subcategory: titleData,
-                });
-            }
+      if (duplicateCheck !== true) {
+        subCategoryCol.set(idData, {
+          id: idData,
+          maincategory: maincategoryData,
+          subcategory: titleData,
+        });
+      }
 
-            return {
-                status: 200,
-                data: subCategoryCol["$dataset"],
-            };
-        },
+      return {
+        status: 200,
+        data: subCategoryCol["$dataset"],
+      };
     },
+  },
 
-    "GET /subcategory": {
-        // middlewares: ["auth"],
-        async handler(req, rep) {
-            const title = "subcategory";
-            const subCategoryCol = Database.sharedInstance().getCollection(title);
-            return {
-                status: 200,
-                header: {},
-                data: subCategoryCol["$dataset"],
-            };
-        },
+  "GET /subcategory": {
+    // middlewares: ["auth"],
+    async handler(req, rep) {
+      const title = "subcategory";
+      const subCategoryCol = Database.sharedInstance().getCollection(title);
+      return {
+        status: 200,
+        header: {},
+        data: subCategoryCol["$dataset"],
+      };
     },
+  },
 };
--- src/routes/type.js
+++ src/routes/type.js
@@ -43,6 +43,50 @@ module.exports = {
     },
   },
 
+  "DELETE /type": {
+    middlewares: ["auth"],
+    async handler(req, rep) {
+      // TODO : type을 List<String>으로 저장
+      const title = "type";
+      const data = req.body[title];
+      // console.log(data);
+
+      const typeCol = Database.sharedInstance().getCollection(title);
+
+      // TODO : mainCategoryCol이 있으면 List로 가져오고, 없으면 빈 값(파일) 생성
+      const getTypeArr = typeCol.get(title);
+      // console.log("getTypeArr", getTypeArr);
+
+      // db가 없으면 error return
+      if (getTypeArr === undefined) {
+        const error = new Error("has no data");
+        error.status = 403;
+        return error;
+      }
+
+      // TODO : 중복된 데이터가 있는지 체크(없으면 -1, 있으면 해당 data)
+      const getType = getTypeArr.findIndex((type) => type === data);
+      console.log("getType", getType);
+
+      // TODO : 중복된 데이터가 없으면 error를 반환
+      if (getType === -1) {
+        const error = new Error("has no data");
+        error.status = 403;
+        return error;
+      }
+
+      // TODO : 중복된 데이터가 있으면 col에서 del
+      getTypeArr.splice(getType, 1);
+      console.log("getTypeArr", getTypeArr);
+      typeCol.set(title, getTypeArr);
+
+      return {
+        status: 200,
+        data: { type: getTypeArr },
+      };
+    },
+  },
+
   "GET /type": {
     async handler(req, rep) {
       const title = "type";
