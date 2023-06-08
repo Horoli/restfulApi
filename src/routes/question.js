f7b2adf0c47a73b0dfccdf90e7ee584abdfbc883
Horoli <sunha321@gmail.com>
2023-06-08 22:22:07 +0900

    update : get/question/wish

--- src/routes/question.js
+++ src/routes/question.js
@@ -6,13 +6,21 @@ module.exports = {
   "POST /question": {
     middlewares: ["auth"],
     async handler(req, rep) {
-      const { question, answer, categoryID, difficulty, score, images, periodID, info, description } =
-        req.body;
+      const {
+        question,
+        answer,
+        categoryID,
+        difficulty,
+        score,
+        images,
+        periodID,
+        info,
+        description,
+      } = req.body;
 
-      if (question === '' || answer === '' || categoryID === '') {
-
-        console.log('question', question === '');
-        const error = new Error('paramater is empty')
+      if (question === "" || answer === "" || categoryID === "") {
+        console.log("question", question === "");
+        const error = new Error("paramater is empty");
         error.status = 400;
         return error;
       }
@@ -35,34 +43,33 @@ module.exports = {
        * graphLook 으로 가져온 데이터는 내림차순으로 정렬되어 있음
        * 따라서 가장 첫 번째에 있는 데이터가 최상위 subCategory이다.
        **/
-      const rootCategoryId = Utility.getrootCategoryId(categoryID);
+      const rootCategoryId =
+        Utility.getRootCategoryFromChildCategory(categoryID);
       Utility.addCounter(rootCategoryId, 1);
 
       // TODO : increase counter
 
-
       return {
         statusCode: 200,
-        data:
-          questionCol.set(questionID, {
-            id: questionID,
-            question: question,
-            answer: answer,
-            updatedAt: Date.now(),
-            createdAt: Date.now(),
-            // TODO : categoryId는 children이 없는 categoryId를 받아야함.
-            // children이 있는 id를 받으면 error
-            categoryID: categoryID,
-            // TODO : difficultyCol 생성 후 클라이언트에서 선택한 difficulty를 입력할 수 있도록 해야함
-            difficulty: difficulty,
-            score: score,
-            // TODO : periodCol 생성해서 id를 입력받음
-            period: [],
-            // TODO : 이미지(base64String) 저장
-            imageIDs: imageIDs,
-            info: info,
-            description: description,
-          }),
+        data: questionCol.set(questionID, {
+          id: questionID,
+          question: question,
+          answer: answer,
+          updatedAt: Date.now(),
+          createdAt: Date.now(),
+          // TODO : categoryId는 children이 없는 categoryId를 받아야함.
+          // children이 있는 id를 받으면 error
+          categoryID: categoryID,
+          // TODO : difficultyCol 생성 후 클라이언트에서 선택한 difficulty를 입력할 수 있도록 해야함
+          difficulty: difficulty,
+          score: score,
+          // TODO : periodCol 생성해서 id를 입력받음
+          period: [],
+          // TODO : 이미지(base64String) 저장
+          imageIDs: imageIDs,
+          info: info,
+          description: description,
+        }),
         // questionID: {
         //   id: questionID,
         //   question: question,
@@ -79,7 +86,6 @@ module.exports = {
         //   period: [],
         //   images: images,
         // }
-
       };
     },
   },
@@ -95,7 +101,6 @@ module.exports = {
 
       const questionValues = Object.values(questionCol["$dataset"]);
 
-
       const filteredQuestion = questionValues.filter(
         (item) => item.categoryID === subCategoryID
       );
@@ -103,7 +108,6 @@ module.exports = {
       console.log("filteredQuestion", filteredQuestion);
 
       const returnValue = filteredQuestion;
-
 
       return {
         data: returnValue,
@@ -114,15 +118,13 @@ module.exports = {
   "GET /question": {
     middlewares: ["auth"],
     async handler(req, rep) {
-      const amount = 20
+      // const amount = 20;
       const questionCol = Database.sharedInstance().getCollection("question");
       const questions = Object.values(questionCol["$dataset"]);
 
-      questions.slice((page - 1) * amount, page * amount)
-
+      // questions.slice((page - 1) * amount, page * amount);
 
       // TODO : amount만
-
 
       return {
         data: questions,
@@ -130,7 +132,7 @@ module.exports = {
     },
   },
 
-  // TODO : /question/random/20을 쿼리하면 20개의 랜덤한 문제를 가져옴 
+  // TODO : /question/random/20을 쿼리하면 20개의 랜덤한 문제를 가져옴
 
   "GET /question/random/:amount": {
     middlewares: ["auth"],
@@ -139,18 +141,14 @@ module.exports = {
       const questionCol = Database.sharedInstance().getCollection("question");
       const questions = Object.values(questionCol["$dataset"]);
 
-      if (amount > 100)
-        amount = 100
-      else if (amount < 10 || amount === undefined)
-        amount = 10
+      if (amount > 100) amount = 100;
+      else if (amount < 10 || amount === undefined) amount = 10;
 
       return {
         data: questions.sort(() => Math.random() - 0.5).slice(0, amount),
       };
-    }
-
+    },
   },
-
 
   // image/id를 입력받으면 해당 id를 가져와
   // buffer로 변환 후 return 해줌
@@ -162,21 +160,19 @@ module.exports = {
       const imageBase64String = Utility.getImage(id);
       const imageBuffer = Buffer.from(imageBase64String, "base64");
       rep.header("Content-Type", "image/png");
-      return imageBuffer
-    }
+      return imageBuffer;
+    },
   },
 
   "PATCH /question": {
     middlewares: ["auth"],
     async handler(req, rep) {
-      const { id, question, answer, categoryID, images, info, description } = req.body;
+      const { id, question, answer, categoryID, images, info, description } =
+        req.body;
 
-
-
-      if (question === '' || answer === '' || categoryID === '') {
-
-        console.log('question', question === '');
-        const error = new Error('paramater is empty')
+      if (question === "" || answer === "" || categoryID === "") {
+        console.log("question", question === "");
+        const error = new Error("paramater is empty");
         error.status = 400;
         return error;
       }
@@ -187,23 +183,21 @@ module.exports = {
 
       const imageIDs = [];
 
-      console.log('getQuestion.imageIDs', getQuestion.imageIDs);
+      console.log("getQuestion.imageIDs", getQuestion.imageIDs);
 
       console.log(getQuestion.imageIDs.length);
 
-      console.log('images.length', images.length);
+      console.log("images.length", images.length);
 
       // TODO : 기존에 저장된 이미지가 있고, 새로운 이미지가 없을 때
       if (images.length === 0 && getQuestion.imageIDs.length !== 0) {
-
         // console.log('image step1');
         imageIDs = getQuestion.imageIDs;
-
       }
 
       // TODO : 기존에 저장된 이미지가 있고, 새로운 이미지가 있을 때
       if (images.length !== 0 && getQuestion.imageIDs.length !== 0) {
-        console.log('image step2');
+        console.log("image step2");
 
         // 기존에 저장된 이미지 삭제
         for (const imageID of getQuestion.imageIDs) {
@@ -216,11 +210,9 @@ module.exports = {
         }
       }
 
-
-
       // TODO : 기존에 저장된 이미지가 없고, 새로운 이미지가 있을 때
       if (images.length !== 0 && getQuestion.imageIDs.length === 0) {
-        console.log('image step3');
+        console.log("image step3");
         for (const image of images) {
           const imageID = Utility.saveImage(image);
           imageIDs.push(imageID);
@@ -243,12 +235,24 @@ module.exports = {
     },
   },
 
-  // TODO : 객관식(multipleChoice) 문제 생성
-  "POST /mcquestion": {
+  // TODO : 클라이언트에서 저장하고 있는 guestId를 쿼리하면
+  // 해당 guest에 저장된 wishQuestion에 저장된 questionId를 활용하여
+  // questionCol에서 가져와서 리턴해 줌
+  "GET /question/wish": {
     middlewares: ["auth"],
     async handler(req, rep) {
+      const guestId = req.token.id;
+
+      const guestCol = Database.sharedInstance().getCollection("guest");
+      const questionCol = Database.sharedInstance().getCollection("question");
+      const guestInfo = guestCol.get(guestId);
+
+      const convertQuestion = guestInfo.wishQuestion.map((e) =>
+        questionCol.get(e)
+      );
+
       return {
-        data: "ok",
+        data: convertQuestion,
       };
     },
   },
